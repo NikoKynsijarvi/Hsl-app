aca63eff2e25287289dc6f6caefa35b3d86b4136
Niko <niktapky@student.jyu.fi>
2022-06-20 14:15:24 +0300

    mapbox conf

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,6 +6,16 @@ import { moveViewport } from "./../reducers/mapReducer";
 import BottomNavigationBar from "./BottomNavigation";
 import { setStation } from "./../reducers/stationsReducer";
 import { setCarPark } from "./../reducers/carParksReducers";
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+
+// The following is required to stop "npm build" from transpiling mapbox code.
+// notice the exclamation point in the import.
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+mapboxgl.workerClass =
+  // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 function Map({ content }) {
   const dispatch = useDispatch();
